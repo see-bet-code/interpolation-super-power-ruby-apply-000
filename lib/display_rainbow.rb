90cec9e3bda8c4c72675f7ed5981b5ff34626c1f
Brittani Taylor <brittani.elan.taylor@gmail.com>
2020-08-19 02:00:56 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
-  puts "#{colors[0][0].upper}: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]},
+  puts "#{colors[0][0].super}: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]},
   B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end
