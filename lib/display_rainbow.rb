3a3c59215adcdb92b12e46939df77e078e59da41
Brittani Taylor <brittani.elan.taylor@gmail.com>
2020-08-19 01:59:23 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,3 +2,4 @@
 def display_rainbow(colors):
   puts "#{colors[0][0].uppercase}: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]},
   B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+end
